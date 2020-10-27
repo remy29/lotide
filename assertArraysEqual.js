9ef80822091602ae215e6ff0e6aa1dd44966bbb2
remy29 <arthur.d.remy@gmail.com>
2020-10-27 22:42:30 +0000

    added assertArraysEqual.js assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  const pusher = function(array) {
+    let string = "";
+    for (let data of array) {
+      string += data.toString();
+    }
+    return string;
+  };
+  
+  let val1 = pusher(array1);
+  let val2 = pusher(array2);
+
+  if (val1 === val2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 6]);
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3']);
+assertArraysEqual(['1', '2', '3'], ['1', '2', '6']);
