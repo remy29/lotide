a28a1967d2c84d5e81600a68de46473f75559ac7
remy29 <arthur.d.remy@gmail.com>
2020-10-27 22:31:21 +0000

    added eqArrays.js assignment

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  const pusher = function(array) {
+    let string = "";
+    for (let data of array) {
+      string += data.toString();
+    }
+    return string;
+  };
+  
+  let val1 = pusher(array1);
+  let val2 = pusher(array2);
+
+  if (val1 === val2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
