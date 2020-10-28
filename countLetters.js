27137c32309ad3bc90ba45fe990f712b3813f409
remy29 <arthur.d.remy@gmail.com>
2020-10-28 21:01:36 +0000

    added countLetters.js functionality

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+
+  const letterCount = {};
+
+  for (const letter of string) {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+assertEqual(countLetters("thissssiivvvdfsgg")['s'], 5);
