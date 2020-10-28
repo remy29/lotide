4f2567f13b537af2fc364f4adbe09f3187cb80bd
remy29 <arthur.d.remy@gmail.com>
2020-10-28 22:14:36 +0000

    added findKeyByValue.js functionality

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const genres in object) {
+    let shows = object[genres].toLowerCase();
+    if (shows === value.toLowerCase()) {
+      return genres;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const aListOfFood = {
+  burgers: "Fatty",
+  salad: "Healthy",
+  ramen: "Delicious"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(aListOfFood, "delicious"), "ramen");
